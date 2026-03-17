6d9c5ab3b8b9aef6dbb9a9122160215bbc5d6393
minju7804124-sys <minju7804124@gmail.com>
2026-03-17 23:01:22 +0900

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,11 @@
+window.SOE_CONFIG = {
+  links: {
+    film1: "https://youtu.be/AoEvTzt7m-M",
+    animation1: "https://youtu.be/DdM62tCoq7I",
+    animation2: "https://youtu.be/nEc8pfo4xWo",
+    commercial1: "https://youtu.be/-tGb15Y5C2E",
+    commercial2: "https://youtube.com/shorts/CbNAe1sOTwM?feature=share",
+    commercial3: "https://youtube.com/shorts/7aNHoI5467U?feature=share",
+    works1: "https://youtu.be/CNheay_Yfj8"
+  }
+};
